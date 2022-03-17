070e3a1e3117f91cd4bf3fc9a25243cac33fe2fd
alien <jamesiwelsh@hotmail.com>
2022-03-17 10:53:02 +0000

    Working

--- src/App.js
+++ src/App.js
@@ -12,12 +12,20 @@ const getRandomDiceRollResult = (diceSides) => {
   return Math.floor(Math.random() * diceSides) + 1;
 }
 
+const MESSAGES = {
+  draw : "Its a draw",
+  player : "Player wins",
+  computer : "Computer Wins"
+}
+
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-        player: [0, 0],
+        counter : 0,
+        player_1: 0,
+        player_2: 0,
         dice_1 : 0,
         dice_2 : 0,
         message : "Click roll to begin"
@@ -26,33 +34,40 @@ class App extends Component {
 
   rollDice = () => {
 
-    /*
     const diceSides = 6;
     const dice = [];
 
     dice[0] = getRandomDiceRollResult(diceSides);
-    this.setState({ dice_1: 1});
+    this.setState({ dice_1: dice[0]});
 
     dice[1] = getRandomDiceRollResult(diceSides);
     this.setState({ dice_2: dice[1]});
-    */
 
-    // update the state
+    let message = MESSAGES.draw;
 
-    // get the score results
+    if(dice[0] > dice[1]){
+      message = MESSAGES.player;
+      this.setState((prevState, props) => ({
+          player_1: prevState.player_1 + 1
+      }));
 
-    // show the message
+    }else if(dice[0] < dice[1]){
+      message = MESSAGES.computer;
+      this.setState((prevState, props) => ({
+          player_2: prevState.player_2 + 1
+      }));
+    }
 
-    this.setState({ message: "Dice has been rolled"});
+    this.setState({ message: message});
 
   }
 
   render(){
-    return (s
+    return (
       <div>
         <div>
-          <div>Player : {this.state.player[0]}</div>
-          <div>Computer : {this.state.player[1]}</div>
+          <div>Player : {this.state.player_1}</div>
+          <div>Computer : {this.state.player_2}</div>
         </div>
         <div className="dice_holder" >
           <div>Player Dice : {this.state.dice_1}</div>
