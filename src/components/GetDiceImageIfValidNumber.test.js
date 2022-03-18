8725791c35ad304f17d8a678cfa4108bf8cb1657
alien <jamesiwelsh@hotmail.com>
2022-03-18 10:38:55 +0000

    Tdiying code

--- src/components/GetDiceImageIfValidNumber.test.js
+++ src/components/GetDiceImageIfValidNumber.test.js
@@ -1,9 +1,14 @@
 import getDiceImageIfValidNumber from './GetDiceImageIfValidNumber';
 
 test('adding a number greater than 6 returns a numeric value', () => {
-  expect(getDiceImageIfValidNumber(7)).toBe(7);
-  let toBeReceived = 'fa-solid fa-5x blackiconcolor fa-dice-six';
+
+  let toBeReceived = 7;
+  expect(getDiceImageIfValidNumber(7)).toBe(toBeReceived);
+
+  toBeReceived = 'fa-solid fa-5x blackiconcolor fa-dice-six';
   expect(getDiceImageIfValidNumber(6)).toBe(toBeReceived);
+
   toBeReceived = 'fa-solid fa-5x blackiconcolor fa-dice-one';
   expect(getDiceImageIfValidNumber(1)).toBe(toBeReceived);
+
 });
